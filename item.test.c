911cb16ff3dbbf084680f5ee125622c81bb9e6de
standardloop <joshuaeli1234@gmail.com>
2026-09-19 12:39:26 +0800

    move item.test.c to testing library

--- item.test.c
+++ item.test.c
@@ -1,5 +1,6 @@
-#include <assert.h>
+#include <_string.h>
 #include <limits.h>
+#include <standardloop/testing.h>
 #include <standardloop/util.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -11,14 +12,19 @@
 
 static void testDuplicate()
 {
-    char *test_string_1 = QuickAllocatedString("teststring1");
-    assert(test_string_1 != NULL);
+    char *test_string_1 = strdup("teststring1");
+    TestCaseVerify(true, "ensure test string is not NULL",
+                   test_string_1 != NULL);
     Item *item = ItemInit(test_string_1, &ItemValueStringOperations);
-    assert(item != NULL);
+    TestCaseVerify(true, "ensure ItemInit returns a non NULL pointer",
+                   item != NULL);
 
     Item *dupe = ItemDuplicate(item);
-    assert(dupe != NULL);
-    assert(strcmp(dupe->value, item->value) == 0);
+    TestCaseVerify(true, "ensure ItemDuplicate returns a non NULL pointer",
+                   dupe != NULL);
+    TestCaseVerify(true, "",
+                   dupe != NULL && item != NULL &&
+                       strcmp(dupe->value, item->value) == 0);
 
     ItemFree(item);
     ItemFree(dupe);
@@ -26,31 +32,41 @@ static void testDuplicate()
 
 static void testToStringSimple()
 {
-    char *test_string_1 = QuickAllocatedString("teststring1");
-    assert(test_string_1 != NULL);
+    char *test_string_1 = strdup("teststring1");
+    TestCaseVerify(true, "Ensure test string is not NULL",
+                   test_string_1 != NULL);
+
     Item *item_1 = ItemInit(test_string_1, &ItemValueStringOperations);
-    assert(item_1 != NULL);
+    TestCaseVerify(true, "Ensure ItemInit returns a non NULL value",
+                   item_1 != NULL);
+
     char *item_value_as_string = ItemToString(item_1);
-    assert(strcmp("\"teststring1\"", item_value_as_string) == 0);
+    TestCaseVerify(true, "ensure ItemToString returns a non NULL value",
+                   item_value_as_string != NULL);
+    TestCaseVerify(true, "ensure ItemToString matches the expected string",
+                   strcmp("\"teststring1\"", item_value_as_string) == 0);
     ItemFree(item_1);
     free(item_value_as_string);
 }
 
 extern void TestItem()
 {
-    char *test_string_1 = QuickAllocatedString("teststring1");
-    assert(test_string_1 != NULL);
+    char *test_string_1 = strdup("teststring1");
+    TestCaseVerify(true, "Ensure test string is not NULL",
+                   test_string_1 != NULL);
     Item *item_1 = ItemInit(test_string_1, &ItemValueStringOperations);
-    assert(item_1 != NULL);
-    assert(strcmp(test_string_1, item_1->value) == 0);
+    TestCaseVerify(true, "Ensure ItemInit doesn't return NULL", item_1 != NULL);
+    TestCaseVerify(true, "Ensure item value matches the test string",
+                   item_1 != NULL && strcmp(test_string_1, item_1->value) == 0);
     ItemFree(item_1);
 
     int *test_int_1 = malloc(sizeof(int));
     *test_int_1 = 42;
-    assert(test_int_1 != NULL);
+    TestCaseVerify(true, "Ensure test int is not NULL", test_int_1 != NULL);
     Item *item_2 = ItemInit(test_int_1, &ItemValueIntOperations);
-    assert(item_2 != NULL);
-    assert(42 == *((int *)item_2->value));
+    TestCaseVerify(true, "Ensure ItemInit returns non NULL", item_2 != NULL);
+    TestCaseVerify(true, "Ensure item value matches the expected int value",
+                   item_2 != NULL && 42 == *((int *)item_2->value));
     ItemFree(item_2);
 
     testToStringSimple();
